84f2c07ad306ba599c6f85ce2ff6abd0c1ab8d4d
Mani Vijaya Raman Theagarajan <80083165+tmmni@users.noreply.github.com>
2022-07-03 12:34:11 +0530

    Delete RandomPasswordGenerator.py

--- RandomPasswordGenerator.py
+++
@@ -1,5 +0,0 @@
-import random
-import string
-(random.choice(string.ascii_lowercase)+random.choice(string.ascii_uppercase)+
- random.choice(string.punctuation)+random.choice(string.digits)+random.choice(string.ascii_lowercase)+
- random.choice(string.ascii_lowercase)+random.choice(string.ascii_letters)+random.choice(string.digits))
